635df960a7e24a57fce694b134524734c4631fa0
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-05-18 17:43:32 +0300

    feat: add fourth task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
 
+(function copyDirectory () {
+  fs.mkdir(
+    path.join(__dirname, 'files-copy'),
+    {recursive: true},
+    err => {
+      if (err) throw err;
+    });
+
+  fs.readdir(
+    path.join(__dirname, 'files-copy'),
+    (err, files) => {
+      if(err) throw err;
+      for (let file of files) {
+        fs.unlink(path.join(__dirname, 'files-copy', file), function(err){
+          if (err) throw err;
+        });
+      }
+    });
+
+  fs.readdir(
+    path.join(__dirname, 'files'),
+    (err, files) => {
+      if(err) throw err;
+      for (let file of files) {
+        fs.copyFile(
+          path.join(__dirname, 'files', file),
+          path.join(__dirname, 'files-copy', file),
+          function (err) {
+            if (err) throw err;
+          });
+      }
+    });
+})();
