ceed61e42c2d8e409c173b79189d35aada5b6cc1
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-05-23 13:35:11 +0300

    fix: use ReadStream for first task solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const file = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-fs.readFile(file, 'utf8', (error, data) => {
-  if (error) throw error;
-  console.log(data);
-});
+let text = '';
+
+stream.on('data', chunk => text += chunk);
+stream.on('end', () => console.log(text));
+stream.on('error', error => console.log('Error', error.message));
